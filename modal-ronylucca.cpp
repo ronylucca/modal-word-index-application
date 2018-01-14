7db979ab696a7287069884edd4e39bb9c8e32ecf
Rony Lucca Oliveira <ronylucca@gmail.com>
2018-01-14 06:33:20 -0200

    correcao do arquivo de palavras | implementacao da solucao

--- modal-ronylucca.cpp
+++ modal-ronylucca.cpp
@@ -15,5 +15,25 @@ int main()
 			continue;
 		}
 
-    }
+char limit='a';
+		int index=1;
+		
+		for(int i=0;i<len;i++){
+			for(char first=limit;first<word[i];first++)
+				index+=count(first,len-i);
+			limit=word[i]+1;
+		}
+
+		for(int i=1;i<len;i++)
+			index+=join(26,i);
+		printf("%d\n",index);
+	}
+	return 0;
 }
+
+//inputfile reader
+
+
+//validate word
+
+//aggregator

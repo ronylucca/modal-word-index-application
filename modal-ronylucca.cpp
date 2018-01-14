ba967dd0835fa5230e887c9511ce6bbf1bffaf95
Rony Lucca Oliveira <ronylucca@gmail.com>
2018-01-14 02:33:20 -0200

    constructing main

--- modal-ronylucca.cpp
+++ modal-ronylucca.cpp
@@ -6,3 +6,14 @@
 #include<cstdio>
 #define _CRT_SECURE_NO_WARNINGS
 
+int main()
+{
+	while(scanf("%s",word)!=EOF){
+		len=strlen(word);
+		if(!validate()){
+			printf("0\n");
+			continue;
+		}
+
+    }
+}
